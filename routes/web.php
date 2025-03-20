dc358a51a358cbfbed938479fd02f4abad1cbaa3
Dylan <100005958.joan23@fje.edu>
2025-03-20 17:20:04 +0100

    recuperar los modelos y migraciones

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ruta para la gincana
+Route::get('/gincana', function () {
+    return view('gincana');
+});
