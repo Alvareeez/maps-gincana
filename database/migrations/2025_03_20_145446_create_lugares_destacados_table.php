7158e753ed160f659ee5bfde4864e1f6797af283
PC-ERIC <eric.alcazarc@gmail.com>
2025-03-24 17:00:32 +0100

    Modificacion BD

--- database/migrations/2025_03_20_145446_create_lugares_destacados_table.php
+++ database/migrations/2025_03_20_145446_create_lugares_destacados_table.php
@@ -12,8 +12,8 @@ return new class extends Migration {
             $table->string('nombre');
             $table->text('descripcion');
             $table->string('direccion');
-            $table->decimal('latitud', 8, 2);
-            $table->decimal('longitud', 8, 2);
+            $table->decimal('latitud', 12, 12);
+            $table->decimal('longitud', 12, 12);
             $table->string('tipoMarcador');
             $table->timestamps();
         });
